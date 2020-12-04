ff0ee67f652b0ff3bc4b6e832fdcc0691bba7dcb
Petar <petarstojkovic92@gmail.com>
2020-12-04 19:55:55 +0100

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,9 +2,15 @@ import React from "react";
 
 // Importing components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
