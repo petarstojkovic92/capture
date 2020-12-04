bbec451a16cd81dc133b5147c656cc9fa2980e5e
Petar <petarstojkovic92@gmail.com>
2020-12-04 19:17:43 +0100

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Importing components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
