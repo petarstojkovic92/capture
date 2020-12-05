20f66273be44a8acc4fb26c7cb173d5a78f4aa6c
Petar <petarstojkovic92@gmail.com>
2020-12-05 17:37:30 +0100

    6. Finished Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,6 +5,7 @@ import home1 from "../img/home1.png";
 
 // Importing Styled
 import styled from "styled-components";
+import { About, Description, Hide, Image } from "../styles";
 
 const AboutSection = () => {
   return (
@@ -39,40 +40,5 @@ const AboutSection = () => {
     </About>
   );
 };
-
-// Styled Components
-
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+// Importing Styled
+import styled from "styled-components";
+import { About } from "../styles";
+
 const FaqSection = () => {
   return (
-    <div className='faq'>
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -15,6 +19,7 @@ const FaqSection = () => {
             cupiditate.
           </p>
         </div>
+        <div className='faq-line'></div>
       </div>
       <div className='question'>
         <h4>Daily Schedule</h4>
@@ -25,6 +30,7 @@ const FaqSection = () => {
             cupiditate.
           </p>
         </div>
+        <div className='faq-line'></div>
       </div>
       <div className='question'>
         <h4>Different Payment Methods</h4>
@@ -35,6 +41,7 @@ const FaqSection = () => {
             cupiditate.
           </p>
         </div>
+        <div className='faq-line'></div>
       </div>
       <div className='question'>
         <h4>What Products do you offer?</h4>
@@ -45,9 +52,43 @@ const FaqSection = () => {
             cupiditate.
           </p>
         </div>
+        <div className='faq-line'></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
