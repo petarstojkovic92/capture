4342173fcf1b336543e86391ab95858b44861dda
Petar <petarstojkovic92@gmail.com>
2020-12-05 16:24:07 +0100

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // Importing pages
 import AboutUs from "./pages/AboutUs";
 
+// Importing Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
 function App() {
   return (
     <div className='App'>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
