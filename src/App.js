faa12c9050d57e1da960dbabf05a9c981432c0ae
Petar <petarstojkovic92@gmail.com>
2020-12-06 17:05:47 +0100

    8. Our Work page and router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // Importing pages
 import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 
 // Importing Global Style
 import GlobalStyle from "./components/GlobalStyle";
+
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path='/'>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,9 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
+// Router
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   return (
     <StyledNav>
@@ -12,13 +15,13 @@ const Nav = () => {
       </h1>
       <ul>
         <li>
-          <a href='#'>1. About Us</a>
+          <Link to='/'>1. About Us</Link>
         </li>
         <li>
-          <a href='#'>2. Our Work</a>
+          <Link to='/work'>2. Our Work</Link>
         </li>
         <li>
-          <a href='#'>3. Contact Us</a>
+          <Link to='/contact'>3. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
