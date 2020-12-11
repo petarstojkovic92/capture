c74d7978be22d5ab11a06f6c2e9295f7e23b59a9
Petar <petarstojkovic92@gmail.com>
2020-12-11 19:20:46 +0100

    10. Working with Motion

--- src/App.js
+++ src/App.js
@@ -9,27 +9,34 @@ import ContactUs from "./pages/ContactUs";
 import GlobalStyle from "./components/GlobalStyle";
 
 // Router
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+// Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route exact path='/'>
-          <AboutUs />
-        </Route>
-        <Route exact path='/work'>
-          <OurWork />
-        </Route>
-        <Route path='/work/:id'>
-          <MovieDetail />
-        </Route>
-        <Route path='/contact'>
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path='/'>
+            <AboutUs />
+          </Route>
+          <Route exact path='/work'>
+            <OurWork />
+          </Route>
+          <Route path='/work/:id'>
+            <MovieDetail />
+          </Route>
+          <Route path='/contact'>
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
