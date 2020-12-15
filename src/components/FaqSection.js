707c32ff84ea64644892ecc483bb79775374dc78
Petar <petarstojkovic92@gmail.com>
2020-12-15 17:15:11 +0100

    16. Scroll Animations

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -6,10 +6,19 @@ import { About } from "../styles";
 import Toggle from "./Toggle";
 // Importing animation
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
+
   return (
-    <Faq>
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial='hidden'
+    >
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
